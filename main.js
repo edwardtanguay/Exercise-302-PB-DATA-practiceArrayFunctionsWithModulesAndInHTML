d6d2e7e2757047a46b6209818cd5e35cced87d32
omer <peynirciomerfaruk@gmail.com>
2022-03-02 22:11:47 +0100

    Setup data modules (customers, orders, products)

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-import { test } from "./test.js";
-console.log(test);
+//import { test } from "./test.js";
+import { customers } from "./customers.js";
+import { orders } from "./orders.js";
+import { products } from "./products.js";
+console.log(`There are ${customers.length} customers.`);
+console.log(`There are ${orders.length} orders.`);
+console.log(`There are ${products.length} products.`);
